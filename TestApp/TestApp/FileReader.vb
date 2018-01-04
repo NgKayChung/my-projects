8355037fc407749c7b77d5f2bab7fc4dcea8c0dc
Ng Kay Chung <ngkay_chung@hotmail.com>
2018-01-04 17:54:20 +0800

    Add files via upload

---
+++ TestApp/TestApp/FileReader.vb
@@ -0,0 +1,34 @@
+﻿Imports System.IO
+
+Public Class FileReader
+    Private FilePath As String
+    Private LineNumber As Integer = 0
+
+    Public Sub New(path As String)
+        FilePath = path
+    End Sub
+
+    Public Sub OpenFile()
+        Dim file = My.Computer.FileSystem.OpenTextFileReader(FilePath)
+        GetLineNumbers(file)
+    End Sub
+
+    Public Sub GetLineNumbers(fileStream As StreamReader)
+        While fileStream.EndOfStream = False
+            fileStream.ReadLine()
+            LineNumber += 1
+        End While
+    End Sub
+
+    Public Function GetData() As String()
+        Dim file = My.Computer.FileSystem.OpenTextFileReader(FilePath)
+        Dim lineData(LineNumber) As String
+        Dim index As Integer = 0
+
+        For index = 0 To lineData.Length - 1
+            lineData(index) = file.ReadLine()
+        Next
+
+        Return lineData
+    End Function
+End Class
---
+++ TestApp/TestApp/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        System.Diagnostics.Process.Start("C:\Users\HAPPY MOMENTS\Documents\testbatch.bat")
+        ReadFile()
+    End Sub
+
+    Private Sub ReadFile()
+        Try
+            Dim FileObj As FileReader = New FileReader("C:\xampp\test.txt")
+            FileObj.OpenFile()
+            Dim data As String() = FileObj.GetData()
+
+            Dim n As Integer = 0
+
+            For n = 0 To data.Length - 1
+                Console.WriteLine("Line " + (n + 1).ToString() + " : " + data(n))
+            Next
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+End Class
