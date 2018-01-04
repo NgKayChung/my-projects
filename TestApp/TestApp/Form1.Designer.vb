906b94e130e6b10cf15d6232156b5fb521ad98ee
Ng Kay Chung <ngkay_chung@hotmail.com>
2018-01-04 18:14:40 +0800

    Add files via upload

--- TestApp/TestApp/Form1.Designer.vb
+++ TestApp/TestApp/Form1.Designer.vb
@@ -27,7 +27,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(94, 76)
+        Me.Button1.Location = New System.Drawing.Point(102, 22)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
--- TestApp/TestApp/Form1.vb
+++ TestApp/TestApp/Form1.vb
@@ -8,13 +8,8 @@
         Try
             Dim FileObj As FileReader = New FileReader("C:\xampp\test.txt")
             FileObj.OpenFile()
-            Dim data As String() = FileObj.GetData()
+            Dim data As String() = FileObj.GetData() 'File Data
 
-            Dim n As Integer = 0
-
-            For n = 0 To data.Length - 1
-                Console.WriteLine("Line " + (n + 1).ToString() + " : " + data(n))
-            Next
         Catch ex As Exception
             MessageBox.Show(ex.Message)
         End Try
