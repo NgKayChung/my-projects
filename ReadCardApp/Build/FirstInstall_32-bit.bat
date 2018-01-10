ecc5d451309b1f8dae3e7dd9c41e72fcc016d5aa
Ng Kay Chung <ngkay_chung@hotmail.com>
2018-01-10 17:38:10 +0800

    Add files via upload

---
+++ ReadCardApp/Build/FirstInstall_32-bit.bat
@@ -0,0 +1,2 @@
+msiexec.exe /i "%~dp0Install\OpenSC-0.17.0-win32_vs12-Release.msi"
+"%~dp0Install\AccessDatabaseEngine.exe"
---
+++ ReadCardApp/Build/FirstInstall_64-bit.bat
@@ -0,0 +1,3 @@
+msiexec.exe /i "%~dp0Install\OpenSC-0.17.0-win64_vs12-Release.msi"
+msiexec.exe /i "%~dp0Install\OpenSC-0.17.0-win32_vs12-Release.msi"
+"%~dp0Install\AccessDatabaseEngine.exe"
